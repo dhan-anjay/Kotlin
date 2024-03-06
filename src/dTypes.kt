c096663d276cd853e57d8e0fbdcc9fc112b0228f
dhan-anjay <dhananjay.kpathak.111@gmail.com>
2024-03-07 01:15:07 +0530

    Initial commit

---
+++ src/dTypes.kt
@@ -0,0 +1,8 @@
+fun dtypes(){
+    var num: Int = 12
+    var num2: Int = 15
+    println(num + num2)
+}
+fun main(){
+    dtypes()
+}
---
+++ src/variables.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var name = "Dhananjay" //var can be reassigned
+    val naam = "Pathak" //val cannot be reassigned
+    name = "Dhiraj"
+    name = "Atul"
+    println(name)
+    println(naam)
+}
